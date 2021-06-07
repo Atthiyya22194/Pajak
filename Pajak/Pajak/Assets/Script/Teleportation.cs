75af5eee609f3d357fb9aca36b02a26e204a51ae
Fiki Abdul Rachman <fiki.abdulrachman@gmail.com>
2021-06-07 20:15:34 +0700

    Add & Delete Feature
    Delete FIle, Add Sprite & Script, Update Scene

---
+++ Pajak/Pajak/Assets/Script/Teleportation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject teleport;
+    public GameObject player;
+    public GameObject buttonSign;
+
+    public bool canTeleport;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && canTeleport)
+        {
+            StartCoroutine(Teleport());
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            canTeleport = true;
+        }
+
+        buttonSign.SetActive(true);
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            canTeleport = false;
+        }
+
+        buttonSign.SetActive(false);
+    }
+    public IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(0.2f);
+        player.transform.position = new Vector2(teleport.transform.position.x, teleport.transform.position.y);
+        StopCoroutine(Teleport());
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-Dummy untuk project
